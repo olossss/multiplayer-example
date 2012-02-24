bf88eaa46e349e4df6f5e7d7fbf7457ea430e7ea
dkok <dkok@dkok.cyestcorp.local>
2012-02-24 11:04:48 +0200

    Added InputManager Added PlayerManager Added Player

---
+++ MultiplayerGame/MultiplayerGame/Entities/Player.cs
@@ -0,0 +1,108 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Player.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MultiplayerGame.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Player : Sprite
+    {
+        private int livesRemaining = 3;
+
+        public int LivesRemaining
+        {
+            get
+            {
+                return this.livesRemaining;
+            }
+
+            set
+            {
+                this.livesRemaining = value;
+            }
+        }
+
+        public bool IsDestroyed { get; set; }
+
+        private bool isInvulnerable;
+        public bool IsInvulnerable
+        {
+            get { return isInvulnerable; }
+            set
+            {
+                if (this.isInvulnerable == value)
+                {
+                    return;
+                }
+
+                isInvulnerable = value;
+                if (this.isInvulnerable)
+                {
+                    this.invulnerableTimer.Reset();
+                }
+            }
+        }
+
+        public int Score { get; set; }
+
+        private readonly GameTimer invulnerableTimer = new GameTimer();
+
+        #region Constructors and Destructors
+
+        internal Player(
+            long id,
+            Texture2D texture,
+            Rectangle initialFrame,
+            int frameCount,
+            int collisionRadius,
+            EntityState simulationState)
+            : base(id, texture, initialFrame, frameCount, collisionRadius, simulationState)
+        {
+        }
+
+        #endregion
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            this.TintColor = (this.IsInvulnerable) ? Color.DarkSlateGray : Color.White;
+
+            base.Draw(spriteBatch);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (this.IsDestroyed)
+            {
+                this.LivesRemaining--;
+
+                if (this.LivesRemaining > 0)
+                {
+                    this.IsDestroyed = false;
+                    this.IsInvulnerable = true;
+                }
+            }
+
+            if (this.IsInvulnerable && this.invulnerableTimer.Stopwatch(1000))
+            {
+                this.IsInvulnerable = false;
+            }
+
+            if (!this.IsDestroyed)
+            {
+                base.Update(gameTime);
+            }
+        }
+    }
+}
---
+++ MultiplayerGame/MultiplayerGame/Managers/InputManager.cs
@@ -0,0 +1,107 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="InputManager.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MultiplayerGame.Managers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class InputManager : GameComponent
+    {
+        #region Constants and Fields
+
+        private readonly ResolutionManager resolutionManager;
+
+        private KeyboardState keyboardState;
+
+        private KeyboardState lastKeyboardState;
+
+        private MouseState lastMouseState;
+
+        private MouseState mouseState;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public InputManager(Game game, ResolutionManager resolutionManager)
+            : base(game)
+        {
+            this.resolutionManager = resolutionManager;
+            this.keyboardState = Keyboard.GetState();
+            this.mouseState = Mouse.GetState();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public Vector2 MousePosition
+        {
+            get
+            {
+                return this.resolutionManager.TransformToViewSpace(new Vector2(this.mouseState.X, this.mouseState.Y));
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Flush()
+        {
+            this.lastKeyboardState = this.keyboardState;
+            this.lastMouseState = this.mouseState;
+        }
+
+        public bool IsKeyDown(Keys keyToTest)
+        {
+            return this.keyboardState.IsKeyDown(keyToTest);
+        }
+
+        public bool IsKeyPressed(Keys keyToTest)
+        {
+            return this.keyboardState.IsKeyUp(keyToTest) && this.lastKeyboardState.IsKeyDown(keyToTest);
+        }
+
+        public bool IsKeyReleased(Keys keyToTest)
+        {
+            return this.keyboardState.IsKeyDown(keyToTest) && this.lastKeyboardState.IsKeyUp(keyToTest);
+        }
+
+        public bool LeftButtonIsClicked()
+        {
+            return this.mouseState.LeftButton == ButtonState.Released &&
+                   this.lastMouseState.LeftButton == ButtonState.Pressed;
+        }
+
+        public bool RightButtonIsClicked()
+        {
+            return this.mouseState.RightButton == ButtonState.Released &&
+                   this.lastMouseState.RightButton == ButtonState.Pressed;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            this.Flush();
+
+            this.keyboardState = Keyboard.GetState(PlayerIndex.One);
+            this.mouseState = Mouse.GetState();
+
+            base.Update(gameTime);
+        }
+
+        #endregion
+    }
+}
